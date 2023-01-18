46f377c7d13e270b9473696fa65c3a0db31ae417
shnya-shibasaki <sksy_al_es@icloud.com>
2023-01-18 23:40:08 +0900

    4th commit

--- aidemy_webapp.py
+++ aidemy_webapp.py
@@ -58,10 +58,10 @@ def upload_file():
 
     return render_template("index.html",answer="")
 
-# if __name__ == "__main__":
-#     port = int(os.environ.get('PORT', 8080))
-#     app.run(host ='0.0.0.0',port = port)
-
-
 if __name__ == "__main__":
-    app.run()    
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host ='0.0.0.0',port = port)
+
+
+# if __name__ == "__main__":
+#     app.run()    
